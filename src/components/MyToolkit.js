846ef83b609d205178f393a10481aa41a07913cf
rishimungia <rishi.mungia@gmail.com>
2021-11-15 17:10:09 +0530

    Added creative-apps image

--- src/components/MyToolkit.js
+++ src/components/MyToolkit.js
@@ -1,4 +1,5 @@
 import techStack from '../data/tech-stack.png';
+import creativeApps from '../data/creative-apps.png';
 
 const MyToolkit = () => {
     return ( 
@@ -8,7 +9,7 @@ const MyToolkit = () => {
                 <div className="toolkit-grid">
                     <div className="pixel" data-aos="fade-up" ><p>Pixel Perfect Code</p></div>
                     <div className="tech-apps" data-aos="fade-up" style={{backgroundImage: `url(`+techStack+`)`}}><p>And Expanding</p></div>
-                    <div className="creative-apps" data-aos="fade-up"></div>
+                    <div className="creative-apps" data-aos="fade-up" style={{backgroundImage: `url(`+creativeApps+`)`}}></div>
                     <div className="pallet" data-aos="fade-up"><p>Creative Pallet</p></div>
                 </div>
             </div>
